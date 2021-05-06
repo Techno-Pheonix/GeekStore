fd894e4783b0c1d1bb157f1b879dc4c2036b5b2a
Mohamed Aziz Hafhouf <hafhoufgeek@gmail.com>
2021-05-06 11:13:43 +0100

    sign in error messages modal

--- web-Project-ISI/signin/index.php
+++ web-Project-ISI/signin/index.php
@@ -63,24 +63,54 @@
 
         </div>
     </div>
-    <div class="d-flex justify-content-center mt-4">
-                    <?php
-                    if (isset($_GET["error"])){
+
+    <button id="a" type="button" class="btn btn-primary" style="background-color:transparent;border-color:transparent;box-shadow: 10px 10px 10px rgba(0, 0, 0, 0);" data-toggle="modal" data-target="#exampleModalLong">
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="exampleModalLongTitle">Error</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <?php
+                            $mtotal="";
+                            if (isset($_GET["error"])){
                                 if ($_GET["error"] == "emptyinput"){
-                                    echo '
-                                    <div class="alert alert-danger" role="alert">There are empty Fields !</div>
-                                    ';
+                                    $mtotal = "There are empty fields !";
                                 }
 
-                                if ($_GET["error"] == "incorrectpwd"){
-                                    echo '
-                                    <div class="alert alert-danger" role="alert">Incorrect Password !</div>
-                                    ';
+                                else if ($_GET["error"] == "incorrectpwd"){
+                                    $mtotal = "Incorrect password !";
+                                   
                                 }
                             }
-
+                            echo('<h4>'.$mtotal.'</h4>');
                             ?>
-                            </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<?php if (isset($_GET["error"])): ?>
+<script>
+    window.onload = function(){
+    document.getElementById('a').click();
+}
+</script>
+<?php endif ?>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+                
     </section>
 </body>
 </html>
