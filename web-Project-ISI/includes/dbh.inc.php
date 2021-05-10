d4b12719e93affc93580c442129f52b8f920cfe5
Mohamed Aziz Hafhouf <hafhoufgeek@gmail.com>
2021-05-10 17:28:29 +0100

    changind dbh home

--- web-Project-ISI/includes/dbh.inc.php
+++ web-Project-ISI/includes/dbh.inc.php
@@ -1,7 +1,7 @@
 <?php 
-$serverName= "197.12.0.100";
-$dbUsername= "user14210_adminf";
-$dbPassword= "fK7pO2qF4gdV1o";
+$serverName= "localhost";
+$dbUsername= "user14210_haffa";
+$dbPassword= "6O3j6F0p";
 $dbName= "user14210_project";
 
 $conn = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
