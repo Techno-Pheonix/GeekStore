2c75cf610d59ceeb3b1af097881a7c0aec92bff2
Mohamed Aziz Hafhouf <hafhoufgeek@gmail.com>
2021-05-11 15:07:40 +0100

    updates

--- web-Project-ISI/includes/header.php
+++ web-Project-ISI/includes/header.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="../home/style.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../admin/assets/css/styles.css">
+    <link rel="stylesheet" href="../admin/assets/bootstrap/css/bootstrap2.min.css">
+    <link rel="stylesheet" href="../admin/assets/bootstrap/css/bootstrap.min.css">
     <link rel="icon" href="../pictures/fav.ico" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
