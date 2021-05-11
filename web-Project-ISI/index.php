2c970acf5415699b2c778594d522ff0ac3ed8572
MoetezWelhazi <welhazi.moetez@gmail.com>
2021-05-11 14:43:56 +0200

    updated navbar

--- web-Project-ISI/index.php
+++ web-Project-ISI/index.php
@@ -6,9 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="home/style.css">
+    <link rel="stylesheet" href="Admin/assets/css/styles.css">
+    <link rel="stylesheet" href="Admin/assets/bootstrap/css/bootstrap2.min.css">
+    <link rel="stylesheet" href="Admin/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="icon" href="pictures/fav.ico" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Home</title>
 </head>
@@ -16,71 +22,9 @@
 
 
 <body>
-    <?php include "includes/dbh.inc.php";
-      session_start(); 
-      if (!isset($_SESSION["shopping_cart"])){
-        $_SESSION["shopping_cart"] = array();
-      }
-    ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a  href="/">
-          <img class="nav-logo" src="pictures/logof2.png" width="80" height="auto" alt="">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <?php 
-            $sql = "SELECT * FROM category";
-            $result = mysqli_query($conn, $sql);?>
-            <?php while ($row = mysqli_fetch_assoc($result)): ?>
-              <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="./browse?catg=<?php echo $row["slug"] ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <?php echo $row['title'] ?>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <?php 
-                $sql_cat = "SELECT sub.* FROM sub_category sub,category where category.id_cat = sub.id_cat and category.id_cat = ".$row['id_cat'].";";
-                $result_cat = mysqli_query($conn, $sql_cat);
-                $resultcheck = mysqli_num_rows($result_cat);
-                if ($resultcheck>0):?>
-                <?php while ($row_cat = mysqli_fetch_assoc($result_cat)): ?>
-                  <a class="dropdown-item" href="./browse?catg=<?php echo $row["slug"] ?>&sub_catg=<?php echo $row_cat["title"] ?>"><?php echo $row_cat["title"] ?></a>
-                <?php endwhile ?>
-                <?php else: ?>
-                  <a class="dropdown-item" href="#">No Sub Categories</a>
-                <?php endif; ?>
-              <?php endwhile ?>
-        </ul>
-
-        <div class="login mr-4"></div>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button id="sbtn" class="btn btn-light my-2 my-sm-0" type="submit"><i class="fas fa-search bs"></i></button>
-        </form>
-      <?php 
-            if (isset($_SESSION["user"])){
-              $username = $_SESSION['user'];
-              if ($_SESSION['admin'] == true){
-                $username = $_SESSION['user']." (admin)";
-              }
-              echo('
-              <h5 class="mr-2 ml-2 id="usern">'.$username.'</h5>
-              <button type="button" class="btn btn-dark btn-sm mr-2 ml-2" onclick="profilered()"><i class="fas fa-user"></i></button>
-              <button type="button" class="btn btn-dark btn-sm" onclick="logoutred()"><i class="fas fa-sign-out-alt"></i></button>
-              ');
-            }
-            else {
-              echo('
-              <button type="button" class="btn btn-dark btn-sm mr-2 ml-2" onclick="signupred()"><i class="fas fa-user-plus"></i></button>
-              <button type="button" class="btn btn-dark btn-sm" onclick="signinred()"><i class="fas fa-sign-in-alt"></i></button>
-              ');
-            }
-          ?>
-      </div>
-    </nav>
+       <?php
+        require_once 'includes/navbar.php';
+       ?>
     <div class="slcontainer d-flex justify-content-center py-1">
     <div class="slider container">
         <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
