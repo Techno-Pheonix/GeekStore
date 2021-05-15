7a8af6f78cb9edcddd568593a01d034919f1a106
Mohamed Aziz Hafhouf <hafhoufgeek@gmail.com>
2021-05-15 11:50:20 +0100

    avatars added

--- web-Project-ISI/faq/index.php
+++ web-Project-ISI/faq/index.php
@@ -12,60 +12,7 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a  href="http://store.hafhouf.com">
-                <img class="nav-logo" src="../pictures/logof2.png" width="80" height="auto" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Tech
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Keyboard</a>
-                      <a class="dropdown-item" href="#">Mouse</a>
-                      <a class="dropdown-item" href="#">Headset</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Trending</a>
-                    </div>
-                  </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Gaming
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Console</a>
-                    <a class="dropdown-item" href="#">Games</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Trending</a>
-                  </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Anime
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="#">Figures</a>
-                      <a class="dropdown-item" href="#">Manga</a>
-                      <a class="dropdown-item" href="#">Posters</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Trending</a>
-                    </div>
-                  </li>
-        
-              </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button id="sbtn" class="btn btn-light my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </div>
-          </nav>
+        <?php require_once 'includes/navbar.php'; ?>
     </header>
     <div class="title text-center mt-4">
         <h1>Frequently Asked Questions</h1>
