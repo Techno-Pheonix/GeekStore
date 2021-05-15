635f71b4c3af020207ee1bcf661159c879302341
yassineraboudi <yassine-raboudi@outlook.com>
2021-05-15 13:31:46 +0100

    payment card

--- web-Project-ISI/payement/payment_shipping.php
+++ web-Project-ISI/payement/payment_shipping.php
@@ -6,29 +6,24 @@
     //Synatize input
     $id_user = mysqli_real_escape_string($conn,$_SESSION["user_id"]);
     $adress1 = mysqli_real_escape_string($conn,$_POST["address"]);
-    
     $adress2 = mysqli_real_escape_string($conn,$_POST["address2"]);
-    
     $country= mysqli_real_escape_string($conn,$_POST["country"]);
     $state = mysqli_real_escape_string($conn,$_POST["state"]);
     $zip = mysqli_real_escape_string($conn,$_POST["zip"]);
     $payement = mysqli_real_escape_string($conn,$_POST["paymentMethod"]);
-
 
     //Insert into Commande table
     $sql = "INSERT into commande
     (`id_user`,`adress1`,`adress2`,`country`,`state`,`zip`,`payment`) 
     VALUES('$id_user','$adress1','$adress2','$country','$state','$zip','$payement')"; //? its a placeholder 
     
-
     mysqli_query($conn, $sql);
     $commande_id = mysqli_insert_id($conn);
 
     foreach($_SESSION["shopping_cart"] as $item){  
-        var_dump($item);
         //Synatize input
         $id_p = mysqli_real_escape_string($conn,$item["item_id"]);
-        $datetime = date("Y-m-d H:i:s");
+        $datetime = date("Y-m-d h:i:sa");
         $quantity = mysqli_real_escape_string($conn,$item["item_quantity"]);
         $total_price = mysqli_real_escape_string($conn,$item["item_price"]);
         $id_c = mysqli_real_escape_string($conn,$commande_id);
@@ -36,11 +31,7 @@
         //Insert into Commande table
         $sql = "INSERT into sales
         (`id_p`,`datetime`,`quantity`,`total_price`,`id_c`) 
-        VALUES('$id_p','$datetime','$quantity','$total_price','$commande_id')"; //? its a placeholder
-        $test = mysqli_query($conn, $sql);
+        VALUES('$id_p','$datetime','$quantity','$total_price','$id_c')"; //? its a placeholder
+        mysqli_query($conn, $sql);
     }
-
-
-
-
 ?>
