514bcdb6b31ef0adcec11f5d5c85edfecd598240
MoetezWelhazi <welhazi.moetez@gmail.com>
2021-05-17 12:46:50 +0200

    Update profile.php

--- web-Project-ISI/Admin/profile.php
+++ web-Project-ISI/Admin/profile.php
@@ -9,13 +9,13 @@ if(isset($_GET['id'])){
 $id = $_GET['id'];
 $sql = "SELECT * from user where id_user = ".$id;
 $_SESSION['perm'] = " readonly=\"readonly\"";
-$_SESSION['url'] = "Profile.php?id=".$id;
+$_SESSION['url'] = "profile.php?id=".$id;
 }
 else
 {
 $sql = "SELECT * from user where id_user =".$_SESSION['user_id'];
 $_SESSION['perm'] = "";
-$_SESSION['url'] = "Profile.php";
+$_SESSION['url'] = "profile.php";
 }
 
 $query = mysqli_query($conn,$sql);
@@ -123,7 +123,7 @@ $row = mysqli_fetch_array($query);
                 </nav>
                 <div class="container-fluid">
                     <h3 class="text-dark mb-4">Profile</h3>
-                    <a  href="<?php echo($_SESSION['url']); ?>&confirm=true">
+                    <a  href="<?php echo($_SESSION['url']."?") ?>confirm=true">
                         <button
                         class="btn btn-primary bg-gradient-deepbluesky bg-gradient-deepbluesky" type="button"
                         style="margin-bottom: 15px;">Delete User</button></a>
